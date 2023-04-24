9f0d247732d9dd0a592dbceed836b1bf298fce99
Wynand van Rensburg <wkvanrensburg@gmail.com>
2023-04-24 16:36:28 +0200

    Add widget to class

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,9 +14,62 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: const Text('BMI CALCULATOR'),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReuseableCard(
+                    color: const Color(0xff1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    color: const Color(0xff1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReuseableCard(
+              color: const Color(0xff1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReuseableCard(
+                    color: const Color(0xff1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReuseableCard(
+                    color: const Color(0xff1D1E33),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class ReuseableCard extends StatelessWidget {
+  ReuseableCard({required this.color});
+
+  Color? color;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        margin: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: color,
+        ),
+      );
 }
